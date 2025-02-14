bf6a3a055ad6c88c72716351820b386e377c23dc
ExecicioEstacio <202502137214@alunos.estacio.br>
2025-02-14 12:05:25 -0300

    fff

---
+++ Olamundo/n1_nc4_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+int idade = 25;
+float altura = 1.75;
+char opção ='S';
+char nome[20] = "renato";
+
+printf("idade: %d\n", idade);
+printf("altura: %e\n", altura);
+printf("opção: %c\n", opção);
+printf("nome: %s\n", nome);
+
+   
+    
+    }
--- Olamundo/olamundo.c
+++ Olamundo/olamundo.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
-int main (){
-printf("Olá, Mundo!\n");
-
-
-
-
-
-
+ 
+int main() {
+    printf("Olá, Mundo!\n");
+    return 0;
 }
